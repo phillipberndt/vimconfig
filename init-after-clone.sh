525830dff39c823f39a0cea237490a5f3ff8d851
Phillip Berndt <phillip.berndt@gmail.com>
2012-01-14 16:13:59 +0100

    Updated initialization script to ask for vim-personal values

--- init-after-clone.sh
+++ init-after-clone.sh
@@ -4,3 +4,11 @@ git submodule init
 git submodule update
 git submodule foreach git checkout master
 python update-scripts.py
+if ! -e vim-personal; then
+	echo "Enter your personal data:"
+	for FIELD in $(cut -d= -f1 vim-personal.template); do
+		echo -ne "$FIELD:\033[15G"
+		read VALUE
+		echo "$FIELD=$VALUE" >> vim-personal
+	done
+fi
