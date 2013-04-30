5557cdf7424d809c8315698f6dc5e2380a3b1f4e
Phillip Berndt <phillip.berndt@googlemail.com>
2013-04-30 11:42:49 +0200

    Remove Python scripts if Python is not supported by the local VIM version

--- init-after-clone.sh
+++ init-after-clone.sh
@@ -23,3 +23,24 @@ if ! -e vim-personal; then
 		echo "$FIELD=$VALUE" >> vim-personal
 	done
 fi
+
+if ! vim --version | grep -q "+python"; then
+	echo
+	echo "You have a VIM version without python support installed."
+	echo "I will now remove everything incompatible:"
+	cd bundle
+	for dir in *; do
+		if [ -d $dir/.git ]; then
+			if grep -Rq ":py " $dir; then
+				rm -rf $dir
+				echo $dir
+			fi
+		else
+			grep -Rl ":py " | while read name; do
+				echo $name
+				rm -f $name
+			done
+		fi
+	done
+	cd ..
+fi
